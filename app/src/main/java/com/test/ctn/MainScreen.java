e7fa6b0becde208290c0bab405516219298b9ce8
Pradeep <choudhary.3@iitj.ac.in>
2017-01-08 03:02:12 +0530

    Modified items fragment

--- app/src/main/java/com/test/ctn/MainScreen.java
+++ app/src/main/java/com/test/ctn/MainScreen.java
@@ -1,5 +1,6 @@
 package com.test.ctn;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,11 @@ public class MainScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        Items items = new Items();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.add(R.id.container, items);
+        ft.commit();
+
     }
 }
