db92506f940d293141e57abe555be6ae3b34c383
Pradeep <choudhary.3@iitj.ac.in>
2017-01-08 02:24:55 +0530

    Modified items fragment

--- app/src/main/java/com/test/ctn/Items.java
+++ app/src/main/java/com/test/ctn/Items.java
@@ -5,11 +5,28 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class Items extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_items, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_items, container, false);
+
+        GridView gridview = (GridView) view.findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(getContext()));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(getContext(), "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
     }
 }
